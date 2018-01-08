345ca6eb05ef010ae377069e5c24bf7cb40eb0f2
pashenkirstein <34997448+pashenkirstein@users.noreply.github.com>
2018-01-09 02:11:53 +0300

    Add files via upload

--- poker_game_generator.py
+++ poker_game_generator.py
@@ -86,8 +86,126 @@ print(h2)
 print(t)
 print("")
 
-# pairs find
-p1 = t.count(h1[0])
-p11 = t.count(h1[1])
-p2 = t.count(h2[0])
-p22 = t.count(h2[0])
+# find combinations
+# creating yours sequence
+g11 = []
+g1 = h1 + t
+print(g1)
+print("")
+# creating opponent's sequence
+g22 = []
+g2 = h2 + t
+print(g2)
+print("")
+
+#finding
+a = 0
+for i in g1:    
+        if i == "2h" or i == "2d" or i == "2c" or i == "2s":
+            a = 2
+            g11.append(2)
+for i in g1:    
+        if i == "3h" or i == "3d" or i == "3c" or i == "3s":
+            a = 2
+            g11.append(3)
+for i in g1:    
+        if i == "4h" or i == "4d" or i == "4c" or i == "4s":
+            a = 2
+            g11.append(4)
+for i in g1:    
+        if i == "5h" or i == "5d" or i == "5c" or i == "5s":
+            a = 2
+            g11.append(5)        
+for i in g1:    
+        if i == "6h" or i == "6d" or i == "6c" or i == "6s":
+            a = 2
+            g11.append(6)          
+for i in g1:    
+        if i == "7h" or i == "7d" or i == "7c" or i == "7s":
+            a = 2
+            g11.append(7)
+for i in g1:    
+        if i == "8h" or i == "8d" or i == "8c" or i == "8s":
+            a = 2
+            g11.append(8)
+for i in g1:    
+        if i == "9h" or i == "9d" or i == "9c" or i == "9s":
+            a = 2
+            g11.append(9)
+for i in g1:    
+        if i == "10h" or i == "10d" or i == "10c" or i == "10s":
+            a = 2
+            g11.append(10)
+for i in g1:    
+        if i == "Jh" or i == "Jd" or i == "Jc" or i == "Js":
+            a = 2
+            g11.append(11)
+for i in g1:    
+        if i == "Qh" or i == "Qd" or i == "Qc" or i == "Qs":
+            a = 2
+            g11.append(12)
+for i in g1:    
+        if i == "Kh" or i == "Kd" or i == "Kc" or i == "Ks":
+            a = 2
+            g11.append(13)
+for i in g1:    
+        if i == "Ah" or i == "Ad" or i == "Ac" or i == "As":
+            a = 2
+            g11.append(14)
+
+
+
+            
+
+for i in g2:    
+        if i == "2h" or i == "2d" or i == "2c" or i == "2s":
+            a = 2
+            g22.append(2)
+for i in g2:    
+        if i == "3h" or i == "3d" or i == "3c" or i == "3s":
+            a = 2
+            g22.append(3)
+for i in g2:    
+        if i == "4h" or i == "4d" or i == "4c" or i == "4s":
+            a = 2
+            g22.append(4)
+for i in g2:    
+        if i == "5h" or i == "5d" or i == "5c" or i == "5s":
+            a = 2
+            g22.append(5)        
+for i in g2:    
+        if i == "6h" or i == "6d" or i == "6c" or i == "6s":
+            a = 2
+            g22.append(6)          
+for i in g2:    
+        if i == "7h" or i == "7d" or i == "7c" or i == "7s":
+            a = 2
+            g22.append(7)
+for i in g2:    
+        if i == "8h" or i == "8d" or i == "8c" or i == "8s":
+            a = 2
+            g22.append(8)
+for i in g2:    
+        if i == "9h" or i == "9d" or i == "9c" or i == "9s":
+            a = 2
+            g22.append(9)
+for i in g2:    
+        if i == "10h" or i == "10d" or i == "10c" or i == "10s":
+            a = 2
+            g22.append(10)
+for i in g2:    
+        if i == "Jh" or i == "Jd" or i == "Jc" or i == "Js":
+            a = 2
+            g22.append(11)
+for i in g2:    
+        if i == "Qh" or i == "Qd" or i == "Qc" or i == "Qs":
+            a = 2
+            g22.append(12)
+for i in g2:    
+        if i == "Kh" or i == "Kd" or i == "Kc" or i == "Ks":
+            a = 2
+            g22.append(13)
+for i in g2:    
+        if i == "Ah" or i == "Ad" or i == "Ac" or i == "As":
+            a = 2
+            g22.append(14)
